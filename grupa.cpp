de006adcdc56749fa440ad7b1b8633ebe198126f
endriu1488 <36673508+endriu1488@users.noreply.github.com>
2019-02-27 22:45:19 +0200

    Add files via upload

---
+++ grupa.cpp
@@ -0,0 +1,20 @@
+#include "GRUPA.h"
+#include <iostream>
+#include <stdlib.h>
+	
+using namespace std;
+void InitGrupa(Grupa &g)
+{
+	cin >> g.id;
+	cin >> g.nr;
+	Student *s= (Student*)malloc(sizeof(Student)*nrS);
+
+}
+0
+void AfisGrupa(Grupa g)
+{
+}
+
+void StergeGrupa(Grupa &g)
+{
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "student.h"
+#include "grupa.h"
+#include <iostream>
+void main()
+{
+	Grupa g;
+	
+	system("pause");
+
+	//InitGrupa(g);                     // citeste studentii 
+	//AfisGrupa(g);
+// afiseaza grupa
+	//StergeGrupa(g);                 // elibereaza spatiul
+}
